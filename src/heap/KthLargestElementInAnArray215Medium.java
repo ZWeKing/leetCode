75741f5820102585d741e58bece05ae2001a2136
weking <wekingzhang@gmail.com>
2019-06-08 16:51:15 +0800

    no215medium

---
+++ src/heap/KthLargestElementInAnArray215Medium.java
@@ -0,0 +1,62 @@
+package heap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 215. Kth Largest Element in an Array
+ * Medium
+ * Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ *
+ * Example 1:
+ *
+ * Input: [3,2,1,5,6,4] and k = 2
+ * Output: 5
+ * Example 2:
+ *
+ * Input: [3,2,3,1,2,4,5,5,6] and k = 4
+ * Output: 4
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ array's length.
+ */
+public class KthLargestElementInAnArray215Medium {
+    public int findKthLargest(int[] nums, int k) {
+
+        //建立最小堆
+        PriorityQueue<Integer> queue = new PriorityQueue<>(k, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+
+                if (o1.compareTo(o2) > 0) {
+                    return 1;
+                } else if (o1.compareTo(o2) == 0) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+
+            }
+        });
+
+        for (int i = 0; i < nums.length; i++) {
+            if(queue.size() < k) {
+                queue.offer(nums[i]);
+            } else {
+                int topValue = queue.peek();
+                if (nums[i] > topValue) {
+                    queue.poll();
+                    queue.offer(nums[i]);
+                }
+            }
+        }
+
+        return queue.poll();
+
+    }
+
+    public static  void main(String[] args) {
+        System.out.println(
+        new KthLargestElementInAnArray215Medium().findKthLargest(new int[]{1,3,5,9,6,8,-1},5)
+        );
+    }
+}
